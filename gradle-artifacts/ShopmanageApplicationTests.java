7fe47ff44698022da9f7546074bfbfba837ddd5a
Sandeep Garg <Sandeep.Garg@sas.com>
2017-02-27 17:00:46 +0530

    gradle artifacts

---
+++ gradle-artifacts/ShopmanageApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sg.sm;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ShopmanageApplication.class)
+public class ShopmanageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
